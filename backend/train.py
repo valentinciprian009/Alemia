be8817c8f796164ac0e9c7b53f4327b6e2ca9414
apostolescus <apostolescus@outlook.com>
2021-02-04 18:39:49 +0200

    Finalizează comparații
    Compară RMSE pentru mai mulți algoritmi de ML.
    Adaugă graficuri pentru documentație.
    
    Signed-off-by: apostolescus <apostolescus@outlook.com>

--- backend/train.py
+++ backend/train.py
@@ -3,13 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, SGDRegressor, Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVR
+import math
 import joblib
 
 WEIGHTS_FILE = "../data/weights.apt"
 
+#Training class
+#Allows for two methods selection - RandomForest and LinearRegression
+#
+#
 
 class Train:
     def __init__(self, check=False):
@@ -40,6 +45,7 @@ class Train:
 
         joblib.dump(self.regressor, WEIGHTS_FILE)
 
+    #test method - plots the difference btw prediction and ground truth
     def test(self, model_name=WEIGHTS_FILE):
 
         self.regressor = joblib.load(model_name)
@@ -50,12 +56,14 @@ class Train:
 
         plt.plot(y_test - y_predicted, marker='o', linestyle='')
         plt.axhline(y=0, color="r", linestyle="-")
-        plt.title("Diferenta nota reala-prezisa RFR")
+        plt.title("Diferenta nota reala-prezisa Linear Regression")
         plt.ylabel("Eroarea de predictie")
         plt.xlabel("Id date test")
-        plt.show()
+        # plt.show()
         plt.savefig("graph.png")
 
+    #compares multiple prediction methods
+    #generates graphic representation of RMSE
     def test_multiple_methods(self):
 
         x_train, y_train = self._get_test_train_data(True)
@@ -64,23 +72,51 @@ class Train:
         linear_regressor = LinearRegression()
         svr_regressor = SVR()
         random_forest_regressor = RandomForestRegressor()
+        ridge_regressor = Ridge()
 
         linear_regressor.fit(x_train, y_train)
         svr_regressor.fit(x_train, y_train)
         random_forest_regressor.fit(x_train, y_train)
+        ridge_regressor.fit(x_train, y_train)
 
         svr_predict = svr_regressor.predict(x_test)
         linear_predict = linear_regressor.predict(x_test)
         random_forest_predict = random_forest_regressor.predict(x_test)
+        ridge_predict = ridge_regressor.predict(x_test)
 
-        print("Linear Predict :", linear_predict)
-        print("True notes: ", y_test)
+        # print("Linear Predict :", linear_predict)
+        # print("True notes: ", y_test)
 
-        print("RMSE SVR: ", self._calculate_rmse(y_test, svr_predict))
-        print("RMSE Linear Regression: ",
-              self._calculate_rmse(y_test, linear_predict))
-        print("RMSE Random Forest: ",
-              self._calculate_rmse(y_test, random_forest_predict))
+        rmse_SVR = self._calculate_rmse(y_test, svr_predict)
+        print("RMSE SVR: ", rmse_SVR)
+
+        rmse_Linear = self._calculate_rmse(y_test, linear_predict)
+        print("RMSE Linear Regression: ", rmse_Linear )
+
+        rmse_Forest = self._calculate_rmse(y_test, random_forest_predict)
+        print("RMSE Random Forest: ", rmse_Forest)
+
+        rmse_Ridge = self._calculate_rmse(y_test, ridge_predict)
+        print("RMSE Ridge: ", rmse_Ridge)
+
+        x = [ 'Linear Reg.', 'Rand. Forest', 'Ridge', 'SVR']
+        y = [ rmse_Linear, rmse_Forest, rmse_Ridge, rmse_SVR]
+
+        counter = 0
+        
+        for i in y:
+            if round(i,4) == 0:
+                x[counter] = x[counter] + "\n" + str(round(i,15))
+            else:
+                x[counter] = x[counter ] + "\n" + str(round(i,4))
+            counter +=1
+
+        plt.bar(x,y)
+        plt.ylabel("Valoare RMSE")
+        plt.xlabel("Algoritm")
+        plt.title("Comparație RMSE")
+        plt.savefig("model_comp.png")
+        plt.show()
 
     def _calculate_rmse(self, y_test, y_predicted):
 
